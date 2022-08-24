47ce1e9654aba38e8f426b22e68ac81dc6e41e1a
JimMoyle <github@jimmoyle.com>
2022-08-24 13:57:59 +0100

    Removed inputobject parameter and parameter set

--- Get-AzWvdMsixPackageExample.ps1
+++ Get-AzWvdMsixPackageExample.ps1
@@ -1,32 +1,30 @@
 function Get-AzWvdMsixPackageExample {
     # Should be Get-AzAvdMsixPackage
-    [CmdletBinding(DefaultParameterSetName = 'Main')]
+    [CmdletBinding()]
 
     Param (
         [Parameter(
-            ParameterSetName = 'Main',
             ValuefromPipelineByPropertyName = $true,
-            ValuefromPipeline = $true,
-            Mandatory = $true
+            ValuefromPipeline = $true
+            #removed mandatory
+            #Running command with no parameters should get all Packages: See Get-AzVM
         )]
         [System.String]$ResourceGroupName,
 
         [Parameter(
-            ParameterSetName = 'Main',
             ValuefromPipelineByPropertyName = $true
         )]
         [System.String]$SubscriptionId,
 
         [Parameter(
-            ParameterSetName = 'Main',
             ValuefromPipelineByPropertyName = $true
         )]
         #This parameter should accept wildcard parameters: https://docs.microsoft.com/en-us/powershell/scripting/developer/cmdlet/supporting-wildcard-characters-in-cmdlet-parameters?view=powershell-7.2
+        #Current  Azure Command list that allow parameters: https://1drv.ms/u/s!Attq59Uc_Owghc8x7EJ59n40-5MxJw?e=O89Xpk
         #Note [] denotes this accepts an array
         [System.String[]]$FullName,
 
         [Parameter(
-            ParameterSetName = 'Main',
             ValuefromPipelineByPropertyName = $true
         )]
         #Note [] denotes this accepts an array
@@ -34,29 +32,26 @@ function Get-AzWvdMsixPackageExample {
         [System.String[]]$HostPoolName,
 
         [Parameter(
-            ParameterSetName = 'Main',
             ValuefromPipelineByPropertyName = $true
         )]
-        [psobject]$DefaultProfile,
+        [psobject]$DefaultProfile
 
+        <#
         [Parameter(
             ParameterSetName = 'ResourceGroupObjectPipeline',
             ValuefromPipeline = $true
         )]
-        #Note [] denotes this accepts an array
-        #Note As ResourceGroup is the mandatory parameter we should be able to pipe by the type which you get from Get-AzResourceGroup
+        Note [] denotes this accepts an array
+        Note As ResourceGroup is the mandatory parameter we should be able to pipe by the type which you get from Get-AzResourceGroup
         [Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResourceGroup[]]$InputObject
+        As we now don't have any mandatory params, taking resource group input as mandatory shouldn't be necessary.
+        #>
     )
 
     begin {
         Set-StrictMode -Version Latest
     } # begin
     process {
-        switch ($PSCmdlet.ParameterSetName) {
-            ResourceGroupObjectPipeline { $InputObject }
-            Main { write-output 'Main' }
-            Default {}
-        }
 
         <#
         We currently have undocumented parameters as part of this cmdlet, I'm not sure if these are intentional or not?
@@ -79,6 +74,9 @@ function Get-AzWvdMsixPackageExample {
         RemoveHostPoolByName <string>
         AddHostPoolByName <string>
         Enable <bool>
+
+        Commands from Azure with non-standard methods are here:
+        https://1drv.ms/u/s!Attq59Uc_Owghc8s4eiWy91qieezAQ?e=1ZrNYD
         #>
 
         <#
